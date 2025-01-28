cbc7151323bcafa97a2dbc445ce968768093552c
RidwanRosling <ridwansbdt@gmail.com>
2025-01-29 00:17:50 +0700

    add about us menu

--- script.js
+++ script.js
@@ -6,9 +6,12 @@ product.forEach((item) => {
   item.addEventListener("click", () => {
     product.forEach((item) => {
       // item.classList.remove("#FFE8B6");
-      item.classList.remove("bg-orange-300");
+      item.classList.remove("bg-lightBrown");
+      item.classList.remove("text-black");
+      item.classList.add("text-slate-50");
     });
-    item.classList.add("bg-orange-300");
+    item.classList.add("bg-lightBrown");
+    item.classList.add("text-black");
 
     if (item.classList.contains("kopikala")) {
       fotoProduct.src = "/img/kopikala.png";
@@ -20,4 +23,20 @@ product.forEach((item) => {
       fotoProduct.src = "/img/susuPisang.png";
     }
   });
+});
+
+// Mobile Menu Toggle
+const hamburger = document.createElement("button");
+hamburger.className = "hamburger fa fa-bars";
+document.querySelector(".nav-list").appendChild(hamburger);
+
+hamburger.addEventListener("click", () => {
+  document.querySelector(".nav-menu").classList.toggle("active");
+});
+
+// Close menu when clicking outside
+document.addEventListener("click", (e) => {
+  if (!e.target.closest(".nav-list")) {
+    document.querySelector(".nav-menu").classList.remove("active");
+  }
 });
