b72604c21345d144abb38b52f12d80af0ffadd19
RidwanRosling <ridwansbdt@gmail.com>
2025-02-06 18:31:37 +0700

    repair script.js because the img doesnt appear when user click menu coffee

--- script.js
+++ script.js
@@ -13,41 +13,51 @@ document.addEventListener("scroll", () => {
 });
 // navbar end
 
+// product.forEach((item) => {
+//   item.addEventListener("click", () => {
+//     product.forEach((item) => {
+//       // item.classList.remove("#FFE8B6");
+//       item.classList.remove("bg-lightBrown");
+//       item.classList.remove("text-black");
+//       item.classList.add("text-slate-50");
+//     });
+//     item.classList.add("bg-lightBrown");
+//     item.classList.add("text-black");
+
+//     if (item.classList.contains("kopikala")) {
+//       fotoProduct.src = "img/kopikala.png";
+//     } else if (item.classList.contains("susu-aren")) {
+//       fotoProduct.src = "img/susuAren.png";
+//     } else if (item.classList.contains("kopi-susu")) {
+//       fotoProduct.src = "img/kopiSusu.png";
+//     } else if (item.classList.contains("susu-pisang")) {
+//       fotoProduct.src = "img/susuPisang.png";
+//     }
+//   });
+// });
 product.forEach((item) => {
   item.addEventListener("click", () => {
-    product.forEach((item) => {
-      // item.classList.remove("#FFE8B6");
-      item.classList.remove("bg-lightBrown");
-      item.classList.remove("text-black");
-      item.classList.add("text-slate-50");
-    });
-    item.classList.add("bg-lightBrown");
-    item.classList.add("text-black");
+    console.log("Menu diklik:", item.classList); // Debugging
 
-    if (item.classList.contains("kopikala")) {
-      fotoProduct.src = "img/kopikala.png";
-    } else if (item.classList.contains("susu-aren")) {
-      fotoProduct.src = "img/susuAren.png";
-    } else if (item.classList.contains("kopi-susu")) {
-      fotoProduct.src = "img/kopiSusu.png";
-    } else if (item.classList.contains("susu-pisang")) {
-      fotoProduct.src = "img/susuPisang.png";
+    product.forEach((menu) => {
+      menu.classList.remove("bg-lightBrown", "text-black");
+      menu.classList.add("text-slate-50");
+    });
+
+    item.classList.add("bg-lightBrown", "text-black");
+
+    // Ambil nama produk berdasarkan class
+    let namaProduk = "";
+    if (item.classList.contains("kopikala")) namaProduk = "kopikala.png";
+    else if (item.classList.contains("susu-aren")) namaProduk = "susuAren.png";
+    else if (item.classList.contains("kopi-susu")) namaProduk = "kopiSusu.png";
+    else if (item.classList.contains("susu-pisang"))
+      namaProduk = "susuPisang.png";
+
+    // Ubah gambar tanpa mengubah path awalnya
+    if (namaProduk) {
+      fotoProduct.src = fotoProduct.src.replace(/[^/]+$/, namaProduk);
+      console.log("Gambar berubah ke:", fotoProduct.src); // Debugging
     }
   });
 });
-
-// // Mobile Menu Toggle
-// const hamburger = document.createElement("button");
-// hamburger.className = "hamburger fa fa-bars";
-// document.querySelector(".nav-list").appendChild(hamburger);
-
-// hamburger.addEventListener("click", () => {
-//   document.querySelector(".nav-menu").classList.toggle("active");
-// });
-
-// // Close menu when clicking outside
-// document.addEventListener("click", (e) => {
-//   if (!e.target.closest(".nav-list")) {
-//     document.querySelector(".nav-menu").classList.remove("active");
-//   }
-// });
